b42cd640f4d2473b516df31ca2d491d0b4b2cc0d
Shoma SATO <noir.neo.04@gmail.com>
2018-04-14 01:49:34 +0900

    いい感じに入力をストリームにするやつ

---
+++ Assets/Scripts/Players/IInputEventProvider.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using UnityEngine;
+
+namespace Players
+{
+    public interface IInputEventProvider
+    {
+        IObservable<bool> GetButtonAsObservable(string buttonName);
+        IObservable<Vector2> GetAxitsAsObservable(string axisXName, string axisYName);
+    }
+}
---
+++ Assets/Scripts/Players/ImputEventImpls/KeyboardMouseInputEventProvider.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UniRx;
+using UniRx.Triggers;
+using UnityEngine;
+
+namespace Players.ImputEventImpls
+{
+    public class KeyboardMouseInputEventProvider : MonoBehaviour, IInputEventProvider
+    {
+        public IObservable<bool> GetButtonAsObservable(string buttonName)
+        {
+            return this.UpdateAsObservable()
+                .Select(_ => Input.GetButton(buttonName))
+                .DistinctUntilChanged();
+        }
+
+        public IObservable<Vector2> GetAxitsAsObservable(string axisXName, string axisYName)
+        {
+            return this.UpdateAsObservable()
+                .Select(_ => new Vector2(Input.GetAxis(axisXName), Input.GetAxis(axisYName)));
+        }
+    }
+}
