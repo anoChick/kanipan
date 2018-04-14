29d44f17b6ef6736a9dced53d0e756c6cd451374
anoChick <anoinchick@gmail.com>
2018-04-14 18:13:12 +0900

    時間のカウントをした。

---
+++ Assets/Scripts/Players/Timer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UniRx;
+using Players;
+using Zenject;
+
+
+public class Timer : MonoBehaviour {
+
+    [SerializeField]
+    private Text timeLabel;
+
+    private PlayerCore playerCore;
+
+    [Inject]
+    private void Initialize(PlayerCore playerCore)
+    {
+        this.playerCore = playerCore;
+    }
+
+    void Start () {
+        Observable.Interval(TimeSpan.FromMilliseconds(500))
+            .TakeUntil(playerCore.ClearedAsObservable())
+            .Subscribe(_ =>
+            {
+                timeLabel.text = (int)Time.realtimeSinceStartup + "秒";
+            })
+            .AddTo(this);
+    }
+}
