349de53e27d1295f07923478c11f5ec42e3ba1f9
anoChick <anoinchick@gmail.com>
2018-04-14 13:15:45 +0900

    ねおりんレビュー対応 rigidbody →_rigidbody 入力の受けた後、FixedUpdateAsObservableを使う

---
+++ Assets/AshiUgokasuYatsu.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Players
+{
+    public class AshiUgokasuYatsu : MonoBehaviour
+    {
+
+        [SerializeField]
+        private Rigidbody2D rigidbody;
+        int piyo = 0;
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.GetKey(KeyCode.Space))
+            {
+                if (piyo == 1)
+                {
+                    return;
+                }
+                piyo = 1;
+                rigidbody.AddTorque(1000f);
+
+            }
+            else
+            {
+                piyo = 0;
+
+            }
+
+        }
+    }
+
+}
